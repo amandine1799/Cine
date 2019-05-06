d1b5f4256f9a41c514804a4d2c943deb08414aaf
Amandine <gaigniere.amandine@live.fr>
2019-05-06 23:47:35 +0200

    Ajout de l'admin

---
+++ admin/header.php
@@ -0,0 +1,17 @@
+<nav>
+    <div class="nav-logo">
+      <a href="../index.php">ALLOCINE<strong>MET</strong></a>
+    </div>
+    <div class="nav-btn">
+      <label for="nav-check">
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+    </div>
+    <input type="checkbox" id="nav-check">
+    <div class="nav-links">
+      <a href="../allo_films.php">films</a>
+      <a href="../contact.php">contact</a>
+    </div>
+</nav>
---
+++ admin/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Contact Allocinemet</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="../css/header.css" />
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Arvo">
+</head>
+
+<body>
+  <?php include('header.php');?>
+  <?php include('../bdd.php');?>
+  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+    </tbody>
+  </table>
+
+</body>
+
+</html>
